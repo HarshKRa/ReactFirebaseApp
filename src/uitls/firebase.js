fd77e552fd21781bee09155eabc2c6fa3c770d4e
Harsh Raj Kumar <harsh726181@gmail.com>
2025-03-15 13:11:46 +0530

    Protected the env variabels, Ready for deployment.

--- src/uitls/firebase.js
+++ src/uitls/firebase.js
@@ -2,14 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+alert(apiKey);
+const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_FIREBASE_APP_ID;
+const measurementId = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBz9BvA44mhioGROarkztqlvhuPPFw2wsw",
-  authDomain: "newcurdproject.firebaseapp.com",
-  projectId: "newcurdproject",
-  storageBucket: "newcurdproject.firebasestorage.app",
-  messagingSenderId: "531591469952",
-  appId: "1:531591469952:web:6af7c71e85cd2ea63cbf65",
-  measurementId: "G-VHK4TMNNQW",
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
 };
 
 // Initialize Firebase
